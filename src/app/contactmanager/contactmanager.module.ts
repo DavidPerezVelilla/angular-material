b0701047d9e0efa06583be4f92dfd206812ace0f
David Perez Velilla <dperezve@emeal.nttdata.com>
2022-04-21 13:46:05 +0200

    dialogs 1

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -12,6 +12,7 @@ import { RouterModule,Routes } from '@angular/router';
 import { UserService } from './service/user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NotesComponent } from './components/notes/notes.component';
+import { NewContactDialogComponent } from './components/new-contact-dialog/new-contact-dialog.component';
 
 
 const routes: Routes=[
@@ -29,7 +30,8 @@ const routes: Routes=[
     ToolbarComponent,
     MainContentComponent,
     SidenavComponent,
-    NotesComponent
+    NotesComponent,
+    NewContactDialogComponent
   ],
   imports: [
     CommonModule,
